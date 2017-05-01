3f64f4e4db0d7bdc19e642847737a37b8f0616ec
Timothy Druley <tim.druley79@gmail.com>
2017-05-01 10:18:35 -0700

    test2

--- php/conn.php
+++ php/conn.php
@@ -3,7 +3,6 @@
 defined('DB_SERVER') ? null : define("DB_SERVER", "localhost");
 defined('DB_USER')   ? null : define("DB_USER", "root");
 defined('DB_PASS')   ? null : define("DB_PASS", "");
-defined('DB_NAME')   ? null : define("DB_NAME", "");
 
 // Create Connection
 $conn = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
