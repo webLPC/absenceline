e6fc177d07b5906a78d5b96a4783cdada5f95ef6
Timothy Druley <tim.druley79@gmail.com>
2017-05-01 10:14:28 -0700

    Update conn.php

--- php/conn.php
+++ php/conn.php
@@ -1,9 +1,9 @@
 <?php
 // Database Constants
 defined('DB_SERVER') ? null : define("DB_SERVER", "localhost");
-defined('DB_USER')   ? null : define("DB_USER", "root");
-defined('DB_PASS')   ? null : define("DB_PASS", "Timd!23");
-defined('DB_NAME')   ? null : define("DB_NAME", "lpc");
+defined('DB_USER')   ? null : define("DB_USER", "");
+defined('DB_PASS')   ? null : define("DB_PASS", "");
+defined('DB_NAME')   ? null : define("DB_NAME", "");
 
 // Create Connection
 $conn = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
